be7a90dc9be911f8f684521b4eff6066919b5525
Nimat13 <nikitamatrosov4@gmail.com>
2023-05-29 18:32:19 +0300

    Задание 2.1 исправлено задание reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -3,10 +3,10 @@ let reveal = document.getElementsByClassName('reveal');
 let isVisible = function(el) {
     let elTop = el.getBoundingClientRect().top;
 
-    if (elTop < 0 && elTop > window.innerHeight) {
-        return false;
-    } else {
+    if (elTop > 0 && elTop < window.innerHeight) {
         return true;
+    } else {
+        return false;
     };
 };
 
@@ -14,8 +14,10 @@ window.addEventListener('scroll', function () {
     for (let i = 0; i < reveal.length; i++) {
         if ( isVisible(reveal[i]) === true) {
             reveal[i].classList.add('reveal_active');
+            console.log(true);
         } else {
             reveal[i].classList.remove('reveal_active');
+            console.log(false);
         };
     };
 });
